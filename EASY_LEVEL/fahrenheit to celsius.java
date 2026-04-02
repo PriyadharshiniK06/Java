57c7ed63667f9c32a2d0e11e77dc2df4e5479bbe
PriyadharshiniK06 <priyadharshini.kumaran06@gmail.com>
2026-04-02 13:51:25 +0530

    Add Fahrenheit to Celsius conversion program

---
+++ EASY_LEVEL/fahrenheit to celsius.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        double f= sc.nextDouble();
+        System.out.println((f-32)*(5.0/9));
+    }
+}
