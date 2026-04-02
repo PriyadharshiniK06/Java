4923c8e01bfec8af2a66fa536625e155942a353a
PriyadharshiniK06 <priyadharshini.kumaran06@gmail.com>
2026-04-02 13:49:33 +0530

    Create celsius to fahrenheit.java

---
+++ EASY_LEVEL/celsius to fahrenheit.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        double c= sc.nextDouble();
+        System.out.println((c*(9.0/5))+32);
+    }
+}
