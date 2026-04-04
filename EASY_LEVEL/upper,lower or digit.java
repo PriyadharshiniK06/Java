82a9bbb7a67b1f6710fa2c845d085ff3d2f108fd
PriyadharshiniK06 <priyadharshini.kumaran06@gmail.com>
2026-04-04 13:08:20 +0530

    Create upper,lower or digit.java

---
+++ EASY_LEVEL/upper,lower or digit.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+         Scanner sc=new Scanner(System.in);
+         char ch=sc.next().charAt(0);
+         if(Character.isUpperCase(ch)){
+             System.out.println("Uppercase");
+         }
+         else if(Character.isLowerCase(ch)){
+             System.out.println("Lowercase");
+         }
+         else if(Character.isDigit(ch)){
+             System.out.println("Digit");
+         }
+         
+    }
+}
