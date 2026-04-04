06fb9bf83eb6d53847da89a5ea19b6e64c000188
PriyadharshiniK06 <priyadharshini.kumaran06@gmail.com>
2026-04-04 13:39:12 +0530

    Create Profit or Loss.java

---
+++ EASY_LEVEL/Profit or Loss.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+         Scanner sc=new Scanner(System.in);
+         int cp=sc.nextInt();
+         int sp=sc.nextInt();
+         if(cp>sp){
+             System.out.println("Loss");
+         }
+         else if(sp>cp){
+             System.out.println("Profit");
+         }
+         else{
+             System.out.println("No Profit No loss");
+         }
+        
+    }
+}
