39e7764770c7aa7af4fa1b34a559f26269ac3129
PriyadharshiniK06 <priyadharshini.kumaran06@gmail.com>
2026-04-05 11:29:54 +0530

    Create sum of digits of N.java

---
+++ EASY_LEVEL/sum of digits of N.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int number=sc.nextInt();
+        int sum=0;
+        while(number>0){
+            int digit=number%10;
+            sum+=digit;
+            number/=10;
+        }
+        System.out.println(sum);
+    }
+}
