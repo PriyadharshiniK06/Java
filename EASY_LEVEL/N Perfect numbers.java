495f95fdfc5512d79b24912c0d17254ccfe6fac9
PriyadharshiniK06 <priyadharshini.kumaran06@gmail.com>
2026-04-06 15:30:30 +0530

    Add program to find perfect numbers
    Implement a program to find and print perfect numbers up to a given input.

---
+++ EASY_LEVEL/N Perfect numbers.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        
+        for(int i=1;i<=a;i++){
+           int divisor=0;
+           for(int j=1;j<=i/2;j++){
+              if(i%j==0){
+                divisor+=j;
+              }
+            }
+            if(divisor==i){
+            System.out.print(i+" ");
+            }
+        }
+    }
+}
