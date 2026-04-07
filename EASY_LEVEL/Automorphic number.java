78b09ef1eb304fa6517a9a13a32fad2746de9185
PriyadharshiniK06 <priyadharshini.kumaran06@gmail.com>
2026-04-07 13:34:28 +0530

    Create Automorphic number.java

---
+++ EASY_LEVEL/Automorphic number.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner (System.in);
+        int num=sc.nextInt();
+        int org=num;
+        int square=num*num;
+        while(num>0){
+            if((num%10)!=(square%10)){
+                System.out.println("Not An automorphic number");
+                return;
+            }
+        }
+        System.out.println("Automorphic number");
+        
+        
+    }
+}
